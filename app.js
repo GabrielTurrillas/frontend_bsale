986e31e6614e14c622c96f8ce6a3ce1826f2199f
Gabriel Turrillas <gabrielturrillasdev@gmail.com>
2021-10-31 11:18:16 -0300

    styling and gallery fill

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
-const getProductBtn = document.getElementById('get-product-btn');
+//SELECTORS
+const gallery = document.getElementById('gallery')
 
-const getData = () => {
-  axios.get('https://backend-bsale.vercel.app/api/product')
-    .then(res => console.log(res.data))
+//EVENT LISTENERS
+//getProductBtn.addEventListener('click', getProductData);
+
+
+//FUNCTIONS
+const getProductData = async () => {
+  const products = await axios.get('https://backend-bsale.vercel.app/api/product')
+    .then(res => res.data)
+  return products
 };
 
-getProductBtn.addEventListener('click', getData);
+const fillGallery = (products) => {
+  console.log(products)
+
+  cards = [];
+  products.forEach((product) => {
+    //Card div
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card')
+
+    //Card title
+    const cardTitle = document.createElement('h3');
+    cardTitle.classList.add('title')
+    cardTitle.innerText = product.name
+    cardDiv.appendChild(cardTitle)
+
+    //Card Img
+    const cardImg = document.createElement('img');
+    cardImg.src = product.url_image
+    cardImg.alt = 'Image not available'
+    cardDiv.appendChild(cardImg)
+
+    // Append card div
+    gallery.appendChild(cardDiv)
+  })
+}
+
+//FUNCTION CALLS
+getProductData().then(products => fillGallery(products))
+
